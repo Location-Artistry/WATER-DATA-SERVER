3f7307556f63a568ba469b9b2e4d63c6519d560f
Location-Artistry <erickerney@gmail/com>
2021-07-26 14:24:54 -0400

    usgsWidget.html dynamic widget

--- functions/index.js
+++ functions/index.js
@@ -18,7 +18,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '1mb'}));
+app.use(express.json({limit: '10mb'}));
 
 class Feature {
     constructor(id) {
